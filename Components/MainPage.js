787dc1dc4eb44a996a5ccdda37c72d21d9fd6929
Arash Heidary <spro.arash@gmail.com>
2018-10-08 17:43:02 +0330

    Main Page Updated

--- Components/MainPage.js
+++ Components/MainPage.js
@@ -3,6 +3,7 @@ import {
   StyleSheet,
   FlatList,
   View,
+  Image,
   TextInput,
   Text,
   Button,
@@ -19,7 +20,29 @@ export default class MainPage extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{ flex: 0.15 }} />
+        <View style={StyleSheet.leftBand}>
+          <View style={StyleSheet.upLeftBand}>
+            <Image
+              style={{ margin: 2 }}
+              source={require("../RES/expand.png")}
+            />
+            <Image
+              style={{ margin: 2 }}
+              source={require("../RES/profile.png")}
+            />
+            <Image
+              style={{ margin: 2 }}
+              source={require("../RES/contact.png")}
+            />
+          </View>
+          <View style={StyleSheet.downLeftBand}>
+            <Image
+              style={{ margin: 2 }}
+              source={require("../RES/setting.png")}
+            />
+            <Image style={{ margin: 2 }} source={require("../RES/about.png")} />
+          </View>
+        </View>
         <FlatList
           data={[
             { key: "Devin" },
@@ -65,5 +88,17 @@ const styles = StyleSheet.create({
     padding: 10,
     fontSize: 18,
     height: 44
-  }
+  },
+  leftBand: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    alignItems: "flex-start"
+  },
+  upLeftBand: {
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-start"
+  },
+  downLeftBand: {}
 });
