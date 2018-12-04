f6848e29fdc4b940f33ba318a6212381b0ffcb62
Arash Heidary <spro.arash@gmail.com>
2018-12-04 11:41:23 +0330

    change profile picture complete

--- Front/MobX/ProfileImageStore.js
+++
@@ -1,9 +0,0 @@
-import { observable } from "mobx";
-
-class ProfileImageStore {
-  @observable
-  image = null;
-}
-
-const profileImageStore = new ProfileImageStore();
-export default profileImageStore;
